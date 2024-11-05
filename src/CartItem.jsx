ad3be0efd9e83f8fe6755d7b3d6f635e3ab09d32
limemedesign <limemedesign@gmail.com>
2024-11-04 22:02:41 -0500

    CartItems component

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,36 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+        CartItem.reduce((total, item) => total + item.quantity * item.cost, 0);
   };
 
   const handleContinueShopping = (e) => {
-   
+        onContinueShopping();
   };
 
-
+  const handleCheckoutShopping = (e) => {
+      alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+        dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+      return item.quantity * item.cost
   };
 
   return (
